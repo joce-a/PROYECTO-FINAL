cebc37ded06e6c97702ea54ab5ddc47b8a622352
Joce Acuña <joce.acuna@gmail.com>
2019-01-15 21:33:26 -0300

    ADD: FIXING DETAILS

--- assets/js/unicorn.js
+++ assets/js/unicorn.js
@@ -5,10 +5,6 @@ $(function (){
 		$('.main_nav__elements').toggleClass('show');
 	});
 
-	//NECESITO CAMBIAR LA IMAGEN DEL UNICORNIO CUANDO HAGA HOVER
-	/*$('.main_about__btn').on('click', function(e) {
-		e.preventDefault();
-		$('.main_about__btn-img').toggleClass('.main_about__btn-img-2');
-	})/* 
+
 
 });
